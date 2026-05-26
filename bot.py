f46b3137430b3ac8fb3fceea331b9dcb6bb15570
Ayush Arya <ayusharyaft63@gmail.com>
2026-05-26 12:11:13 +0530

    Add/update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-COGS = ["cogs.welcome", "cogs.autonick", "cogs.embed"]
+COGS = ["cogs.welcome", "cogs.autonick", "cogs.embed", "cogs.ticket"]
 
 @bot.event
 async def on_ready():
