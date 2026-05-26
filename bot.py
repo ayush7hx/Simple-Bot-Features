1757ae917ad9300d695e689e12cf2447f06a1338
Ayush Arya <ayusharyaft63@gmail.com>
2026-05-26 12:13:24 +0530

    Add keep_alive server for UptimeRobot

--- bot.py
+++ bot.py
@@ -3,8 +3,10 @@ import asyncio
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+from keep_alive import keep_alive
 
 load_dotenv()
+keep_alive()
 
 TOKEN = os.getenv("TOKEN")
 
